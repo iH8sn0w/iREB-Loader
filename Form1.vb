262cdd910eae88f9247675b943f6fe3e63bd1557
iH8sn0w <ih8sn0wydaz@gmail.com>
2010-11-14 20:54:33 -0500

    iREB Loader v1.0

---
+++ Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Public temppath As String
+    Public GoGoGadgetLoader As String
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Opacity = 0
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        temppath = System.Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) & "\Temp"
+        temppath = temppath & "\iREB"
+        Folder_Delete(temppath)
+        Create_Directory(temppath)
+        If File_Exists(temppath & "\iREB.exe") = True Then
+            Application.Exit()
+        Else
+            SaveToDisk("iREB.exe", temppath & "\iREB.exe")
+            GoGoGadgetLoader = temppath & "\iREB.exe"
+            Dim Go As New ProcessStartInfo(GoGoGadgetLoader)
+            Go.WorkingDirectory = temppath
+            Process.Start(Go)
+            Me.Dispose()
+        End If
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("iREB")> 
+<Assembly: AssemblyDescription("iTunes Restore Error Bypasser")> 
+<Assembly: AssemblyCompany("iH8sn0w")> 
+<Assembly: AssemblyProduct("iREB")> 
+<Assembly: AssemblyCopyright("Copyright ©  2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d3c1f4cc-e18c-432c-912e-0304f03fbbc6")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("4.0.0.0")> 
+<Assembly: AssemblyFileVersion("4.0.0.0")> 
